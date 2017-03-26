60bff03feedbbbeedef4e7083c93351abf2c56d3
YongzhengHuang <AzenHuang@163.com>
2017-03-26 18:49:39 +0800

    add MemorySafeAsyncTask

--- app/src/main/java/com/github/azenhuang/praticedemo/activity/AsyncTaskActivity.java
+++ app/src/main/java/com/github/azenhuang/praticedemo/activity/AsyncTaskActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.ProgressBar;
 
 import com.github.azenhuang.praticedemo.R;
+import com.github.azenhuang.praticedemo.util.MemorySafeAsyncTask;
 import com.github.azenhuang.praticedemo.util.SafeAsyncTask;
 
 /**
@@ -42,13 +43,15 @@ public class AsyncTaskActivity extends AppCompatActivity {
         findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new MySafeAsyncTask().execute((Void) null);
+//                new MySafeAsyncTask().execute((Void) null);
+                new MyMemorySafeAsyncTask().execute((Void) null);
             }
         });
         findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new MySafeAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, (Void) null);
+//                new MySafeAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, (Void) null);
+                new MyMemorySafeAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, (Void) null);
             }
         });
 
@@ -120,7 +123,42 @@ public class AsyncTaskActivity extends AppCompatActivity {
         @Override
         protected void onProgressUpdate(Integer... values) {
             progressBar.setProgress(values[0]);
-            Log.d(TAG, this.toString() + ">>>" +  progress);
+            Log.d(TAG, this.toString() + ">>>" + progress);
+        }
+    }
+
+    private class MyMemorySafeAsyncTask extends MemorySafeAsyncTask<Void, Integer, Void> {
+        int progress = 0;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            publishProgress(progress);
+            try {
+                while (progress <= 100) {
+                    Thread.sleep(SLEEP_TIME);
+                    progress += 10;
+                    publishProgress(progress);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progressBar.setProgress(values[0]);
+            Log.d(TAG, this.toString() + ">>>" + progress);
         }
     }
 }
